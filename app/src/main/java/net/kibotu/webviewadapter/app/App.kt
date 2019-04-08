ca4db6bf04580887b33eec05539249ddb257505e
Jan Rabe <jan.rabe@exozet.com>
2019-04-08 09:33:11 +0200

    fixed multidex

--- app/src/main/java/net/kibotu/webviewadapter/app/App.kt
+++ app/src/main/java/net/kibotu/webviewadapter/app/App.kt
@@ -1,13 +1,13 @@
 package net.kibotu.webviewadapter.app
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import net.kibotu.logger.Logger
 
 
 /**
  * Created by [Jan Rabe](https://about.me/janrabe).
  */
-class App : Application() {
+class App : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
