a1f96c7afcebfca127f1d5a08152857b05ec5be1
Jan Rabe <jan.rabe@exozet.com>
2019-04-08 09:18:10 +0200

    added debug log

---
+++ webviewadapter/src/main/java/net/kibotu/webviewadapter/DebugExtensions.kt
@@ -0,0 +1,21 @@
+/**
+ * Created by [Jan Rabe](https://about.me/janrabe).
+ */
+
+@file:JvmName("DebugExtensions")
+
+package net.kibotu.webviewadapter
+
+import android.util.Log
+
+internal val debug = false
+
+internal fun Any.log(message: String?) {
+    if (debug)
+        Log.d(this::class.java.simpleName, "$message")
+}
+
+internal fun Exception.log(message: String?) {
+    if (debug)
+        Log.d(this::class.java.simpleName, "$message")
+}
